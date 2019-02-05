8282ef266514609e0e8eeb68276450de6a146ab1
EC2 Default User <ec2-user@ip-172-31-47-91.us-east-2.compute.internal>
2019-02-05 19:26:59 +0000

    Small fix in step1.sh and gitignore

--- step1.sh
+++ step1.sh
@@ -4,7 +4,7 @@
 # run this for every user upon file upload
 # ./step1.sh file_path filename
 
-file_path = $1
-filename = $2
+file_path=$1
+filename=$2
 
 Rscript scripts/prepare_23andme_genome.R file_path filename > logs/warnings.logs
