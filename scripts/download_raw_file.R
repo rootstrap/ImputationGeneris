3ff165e51c4fc135729286265cc2007482c0c4c4
German Lopez <german@rootstrap.com>
2019-02-19 10:58:45 -0300

    Remove unused wrapper

--- scripts/download_raw_file.R
+++ scripts/download_raw_file.R
@@ -5,8 +5,8 @@ download_raw_file<-function(url, upload_id) {
   file_name <- paste('upload_', upload_id, '.', extension, sep="")
   file_path <- paste('raw_files/', file_name, sep="")
   print("Downloading file")
-  download.file(url, file_path, quiet = FALSE, cacheOK = TRUE, headers = NULL)
+  download.file(url, file_path, quiet=FALSE, cacheOK=TRUE, headers=NULL)
   print("Download finished")
   prepare_23andme_genome(file_path, file_name)
-  print("Starting imputation preprocess")
+  print("Preparing file format")
 }
