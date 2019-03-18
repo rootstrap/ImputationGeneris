b3907ecdb69b52fe333859db3d8abf157b0035e3
German Lopez <german@rootstrap.com>
2019-03-18 14:58:43 -0300

    Move download file function to different file

---
+++ scripts/download_remote_file.R
@@ -0,0 +1,14 @@
+source('scripts/prepare_23andme_genome.R')
+
+download_remote_file<-function(url, upload_id) {
+  setwd('.')
+  #Filename will be upload_x.extension
+  extension <- tools::file_ext(sub("\\?.+", "", url))
+  file_name <- paste('upload_', upload_id, '.', extension, sep="")
+  file_path <- paste('raw_files/', file_name, sep="")
+  print('Downloading file')
+  download.file(url, file_path, quiet=FALSE, cacheOK=TRUE, headers=NULL)
+  print('Download finished')
+  prepare_23andme_genome(file_path, file_name, upload_id)
+  print('Preparing file format')
+}
