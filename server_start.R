478c8f293ac32a3c23797c9c3921998d5d4be63d
German Lopez <german@rootstrap.com>
2019-03-19 15:10:41 -0300

    Fix wd in server start script

--- server_start.R
+++ server_start.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 # Starts Plumber server
 
-setwd('.')
+setwd('/home/ec2-user/ImputationGeneris/')
 library(plumber)
 r <- plumb('plumber.R')
 r$run(host = '0.0.0.0', port = 8000)
