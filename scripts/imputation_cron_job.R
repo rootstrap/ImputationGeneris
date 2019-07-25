f29eaafae7192534c5bb37b0a3ac70f4e6c99a01
German Lopez <german@rootstrap.com>
2019-07-25 15:57:25 -0300

    Add error handling to imputation process

--- scripts/imputation_cron_job.R
+++ scripts/imputation_cron_job.R
@@ -5,7 +5,6 @@
 # crontab -u ubuntu -e
 # 50 * * * * Rscript scripts/imputation_cron_job.R > misc_files/cron_logs/`date +\%Y\%m\%d\%H\%M\%S`-impute-cron.log 2>&1
 
-
 #library("mailR")
 library("rJava")
 library("tools")
@@ -32,7 +31,6 @@ for(folderToCheck in foldersToCheck){
 if(runningJobCount > (maxImputations-1)){
   stop(paste("Found", runningJobCount,"running jobs, and max is",maxImputations,"so doing nothing"))
 }
-
 
 #money saving implementation. If this is hub and there's a job, just send an email an turn on a node server. That way server can run on a small computer
 # if(serverRole== "Hub"){
@@ -115,77 +113,15 @@ if(is.na(imputeThisFolder)){
   stop("No folders were found to be ready for imputation")
 }
 
-#If script is still running, it means there was a job ready for imputation - 
-runDir <- paste("imputations/",imputeThisFolder,sep="")
-setwd(runDir)
-load("variables.rdata")
-rawdata <- paste(uniqueID, "_raw_data.txt", sep="")
-
-#if running as node, we also create the output dir already here
-if(serverRole== "Node"){
-  dir.create(paste("data/",uniqueID,sep=""))
-}
-
-#run the imputation
-run_imputation(uniqueID=uniqueID, rawdata=rawdata)
-
-#summarizing files
-destination <- paste0(homePath, "data")
-dir.create(destination)
-output_files <- summarize_imputation(uniqueID=uniqueID,destinationDir=destination)
-
-#Storing relevant output to S3
-remote_file_location <- upload_output_to_S3(upload_id = uniqueID, local_file_path = output_files[1])
-if (!is.null(remote_file_location)) {
-  full_url <- paste0("https://s3.", region, ".amazonaws.com/", bucketName, remote_file_location)
-  notify_api_after_imputation(upload_id = uniqueID, output_url = full_url)
-} else {
-  exit_on_fail(paste("Something went wrong uploading to S3:", uniqueID))
-}
-
-#If this is running as a node, we need to copy it back around here
-if(serverRole== "Node"){
-  cmd5 <- paste("scp -r data/",uniqueID, admin,"@",hubAddress,":data",sep="")
-  system(cmd5)
-}
-
-doneProcessFolder <- paste0(homePath, "done/")
-#making a link out to where the data can be retrieved	(different on hub and node)
-if(serverRole== "Node"){
-  cmd6 <- paste("ssh ", admin,"@",hubAddress," 'ln -s data/",uniqueID,"/",uniqueID,".simple_format.zip ", doneProcessFolder, uniqueID,".simple_format.zip'",sep="")
-  system(cmd6)
-  
-  cmd7 <- paste("ssh ", admin,"@",hubAddress," 'ln -s data/",uniqueID,"/",uniqueID,".gen.zip", doneProcessFolder, uniqueID,".gen.zip'",sep="")
-  system(cmd7)
-  
-  # cmd8 <- paste("ssh ", admin,"@",hubAddress," 'ln -s data/",uniqueID,"/",uniqueID,"_data.json done/",uniqueID,"_data.json'",sep="")
-  # system(cmd8)
-  
-  
-}else if(serverRole== "Hub"){
-  file.symlink(
-    from = paste(destination, "/", uniqueID,"/",uniqueID,".simple_format.zip",sep=""),
-    to = paste(doneProcessFolder, uniqueID,".simple_format.zip",sep="")
-  )
-  file.symlink(
-    from = paste(destination, "/", uniqueID,"/",uniqueID,".gen.zip",sep=""),
-    to = paste(doneProcessFolder, uniqueID,".gen.zip",sep="")
-  )
-  # file.symlink(
-  #   from = paste("data/",uniqueID,"/",uniqueID,"_data.json",sep=""),
-  #   to = paste("done/",uniqueID,"_data.json",sep="")
-  # )
-}else{stop("very odd")}
-
-setwd(homePath)
-unlink(runDir,recursive=TRUE)
-unlink(paste0("raw_files/upload_", uniqueID, ".*"))
-
-#also clear the hub imputation_folder if running as node
-if(serverRole== "Node"){
-  cmd9 <- paste("ssh ", admin,"@",hubAddress," 'rm -r imputations/imputation_folder_",uniqueID,"'",sep="")
-  system(cmd9)
-  
-  #also don't leave the finished data here
-  unlink(paste("data/",uniqueID,sep=""),recursive=TRUE)
-}
+logs_file <- paste0(homePath, LOGS)
+# Capturing errors to retry and communicate admins
+tryCatch({
+  single_imputation_run(imputeThisFolder, uniqueID)
+}, error = function(error_message) {
+  write_logs(logs_file, paste("The imputation process failed. The error was:", error_message, "ID:", uniqueID))
+  setwd(homePath)
+  single_imputation_run(imputeThisFolder, uniqueID)
+}, warning = function(warning_message) {
+  write_logs(logs_file, paste("The imputation process finished with warnings:", warning_message, "ID:", uniqueID))
+  return(TRUE)
+})
