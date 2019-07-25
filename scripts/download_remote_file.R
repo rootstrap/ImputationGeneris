157a6dc064a4f353a91ed96e8d9baf89c989640a
German Lopez <german@rootstrap.com>
2019-07-25 16:51:19 -0300

    Track errors during download and preparation

--- scripts/download_remote_file.R
+++ scripts/download_remote_file.R
@@ -1,4 +1,6 @@
 source('scripts/prepare_23andme_genome.R')
+source('misc_files/config.r')
+source("scripts/custom_functions.R")
 
 download_remote_file<-function(url, upload_id) {
   setwd('.')
@@ -6,9 +8,13 @@ download_remote_file<-function(url, upload_id) {
   extension <- tools::file_ext(sub("\\?.+", "", url))
   file_name <- paste('upload_', upload_id, '.', extension, sep="")
   file_path <- paste('raw_files/', file_name, sep="")
-  print('Downloading file')
-  download.file(url, file_path, quiet=FALSE, cacheOK=TRUE, headers=NULL)
-  print('Download finished')
-  prepare_23andme_genome(file_path, file_name, upload_id)
-  print('Preparing file format')
+  tryCatch({
+    print('Downloading file')
+    download.file(url, file_path, quiet=FALSE, cacheOK=TRUE, headers=NULL)
+    print('Download finished')
+    print('Preparing file format')
+    prepare_23andme_genome(file_path, file_name, upload_id)
+  }, error = function(error_message) {
+    write_logs(LOGS, paste("File format preparation failed for:", file_name, "The error was:", error_message))
+  })
 }
